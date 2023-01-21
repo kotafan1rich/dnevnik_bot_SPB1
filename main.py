ac54f919558612b17e410ffefb3ec85fc4007921
kotafan1rich <lesapond2007@gmail.com>
2023-01-21 23:28:28 +0300

    2.3

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from aiogram.utils import executor
 from create_bot import dp
 from handlers import client
 
+import os
+
+try:
+    os.rmdir('~/dnevnik_bot_SPB1')
+except OSError:
+    pass
+
 client.register_handlers_client(dp)
 
 executor.start_polling(dp, skip_updates=True)
